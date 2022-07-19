af23da695625cb488d69db563b6860bb8c9a30d6
Rafael <rafapgbr@gmail.com>
2022-07-19 10:42:48 -0300

    feat: inserção de controller para testar se o banco de dados e a aplicação está configurada corretamente.

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import {AppDataSource} from './data-source';
+
+AppDataSource.initialize().then(() => { //Inicializando o banco de dados antes de configurarmos o express, evitando que qualquer chamada para a API seja realizada sem que o banco de dados seja inicializado.
+    const app = express()
+
+    app.use(express.json())
+
+    app.get('/', (req, res) => {
+        return res.json('Everything is OK.');
+    })
+
+    return app.listen(process.env.PORT);
+})
