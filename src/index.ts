22e8bc39f0890c08b0ce963c81fe6fe27e943a54
Rafael <rafapgbr@gmail.com>
2022-07-19 23:26:55 -0300

    feat: inserção de novas configurações no projeto.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import {AppDataSource} from './data-source';
+import routes from "./routes";
 
 AppDataSource.initialize().then(() => { //Inicializando o banco de dados antes de configurarmos o express, evitando que qualquer chamada para a API seja realizada sem que o banco de dados seja inicializado.
-    const app = express()
+    const app = express();
 
-    app.use(express.json())
+    app.use(express.json());
+
+    app.use(routes);
 
     app.get('/', (req, res) => {
         return res.json('Everything is OK.');
